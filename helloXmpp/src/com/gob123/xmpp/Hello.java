5007ce265ad15e25d6ed4cd45bb7e1cea24be542
wangpenglin@sina.com <wangpenglin@sina.com>
2016-02-29 14:33:41 +0800

    dddd

--- helloXmpp/src/com/gob123/xmpp/Hello.java
+++ helloXmpp/src/com/gob123/xmpp/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello xmpp!");
+		System.out.println("hello again....");
 	}
 
 }
